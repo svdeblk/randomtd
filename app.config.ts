29ab727e598ac3586a31cf3d2041971572a7d716
SVDE <svde.blk@gmail.com>
2024-05-22 11:01:23 -0700

    update

--- app.config.ts
+++ app.config.ts
@@ -5,8 +5,8 @@ export default defineAppConfig({
     description: 'a collection of disparate special interests.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: 'svdeblk',
+      github: 'svdeblk/docus',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
@@ -17,7 +17,7 @@ export default defineAppConfig({
       dir: '.starters/default/content',
       branch: 'main',
       repo: 'docus',
-      owner: 'nuxt-themes',
+      owner: 'svdeblk',
       edit: true
     },
     aside: {
